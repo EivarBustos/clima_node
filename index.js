f2d818d8000a6d817aa1a4569c8f2e4f31f24109
Eivar Bustos <eivarbc@gmail.com>
2023-05-03 16:06:06 +0200

    Se modifico el archivo busqueda implementando url para listar las ciudades, se modifico archivo gitignore para evitar subir .env

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config({path:'./token.env'});
+
 import { inquirerMenu, leerInput, pausa } from "./helper/inquirer.js"
+import { Busquedas } from "./models/busqueda.js";
+
+
+
 console.clear();
 const main = async()=>{
+    const busquedas = new Busquedas();
     let opt ='';
     do{
             // Imprimir menu 
      opt= await inquirerMenu();
      switch(opt){
-        case '1':
-            console.log(" selecciono la 1")
+        case 1:
+            //Mostrar mensaje para escribir 
+            const lugar = await leerInput('Ciudad :');
+            //await porque se va a trabajar cn una Promesa
+            await busquedas.ciudad(lugar);
+            // Buscar los lugares
+            // Seleccionar el lugar 
+            // Mostrar los resultados
+            console.log(`\n Informacion de la ciudad\n`.green);
+            console.log('Ciudad :');
+            console.log('Latitud :');
+            console.log('Longitud :');
+            console.log('Temperatura :');
+            console.log('Temperatura Minima :')
+            console.log('Temperatura Maxima:')
+
+
 
 
             break;
-        case '2':
+        case 2:
             console.log(" selecciono la 2")
             
             break;
